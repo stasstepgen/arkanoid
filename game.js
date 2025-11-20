5942c2b7fbb520b7c17e1b4f3075690b1394ab2a
Stanislav Stepanov <stanislav.stepanov@ivkhk.ee>
2025-11-20 18:30:42 +0200

    platform moving

--- game.js
+++ game.js
@@ -11,17 +11,33 @@ let game = {
         platform: null,
         block: null
     },
+
     init: function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
+        this.setEvents();
     },
+
+    setEvents() {
+        window.addEventListener("keydown", e => {
+            if (e.keyCode === 37) {
+                this.platform.dx = -this.platform.velocity;
+            } else if (e.keyCode === 39) {
+                this.platform.dx = this.platform.velocity;
+            }
+        });
+
+        window.addEventListener("keyup", () => {
+            this.platform.dx = 0;
+        });
+    },
+
     preload(callback) {
         let loaded = 0;
         let required = Object.keys(this.sprites).length;
+
         let onImageLoad = () => {
-            ++loaded;
-            if (loaded >= required) {
-                callback();
-            }
+            loaded++;
+            if (loaded >= required) callback();
         };
 
         for (let key in this.sprites) {
@@ -30,6 +46,7 @@ let game = {
             this.sprites[key].addEventListener("load", onImageLoad);
         }
     },
+
     create() {
         for (let row = 0; row < this.rows; row++) {
             for (let col = 0; col < this.cols; col++) {
@@ -40,17 +57,36 @@ let game = {
             }
         }
     },
+
+    update() {
+        this.platform.move();
+    },
+
     run() {
         window.requestAnimationFrame(() => {
+            this.update();
             this.render();
+            this.run();
         });
     },
 
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
-            this.ball.x, this.ball.y, this.ball.width, this.ball.height);
-        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+
+        this.ctx.drawImage(
+            this.sprites.ball,
+            0, 0,
+            this.ball.width, this.ball.height,
+            this.ball.x, this.ball.y,
+            this.ball.width, this.ball.height
+        );
+
+        this.ctx.drawImage(
+            this.sprites.platform,
+            this.platform.x,
+            this.platform.y
+        );
+
         this.renderBlocks();
     },
 
@@ -69,6 +105,8 @@ let game = {
     }
 };
 
+
+// === ball ===
 game.ball = {
     x: 320,
     y: 280,
@@ -76,12 +114,23 @@ game.ball = {
     height: 20
 };
 
+
+// === platform ===
 game.platform = {
+    velocity: 6,
+    dx: 0,
     x: 280,
-    y: 300
+    y: 300,
+
+    move() {
+        if (this.dx) {
+            this.x += this.dx;
+        }
+    }
 };
 
+
+// === start ===
 window.addEventListener("load", () => {
     game.start();
 });
-
