1c45ee967e56033e8b649cacba9df72ce595d513
Stanislav Stepanov <stanislav.stepanov@ivkhk.ee>
2025-12-01 18:36:52 +0200

    number 12

--- game.js
+++ game.js
@@ -5,114 +5,117 @@ const KEYS = {
 };
 
 let game = {
-    ctx: null,
-    platform: null,
+  ctx: null,
+  platform: null,
+  ball: null,
+  blocks: [],
+  rows: 4,
+  cols: 8,
+  width: 640,
+  height: 360,
+  sprites: {
+    background: null,
     ball: null,
-    blocks: [],
-    rows: 4,
-    cols: 8,
-    width: 640,
-    height: 360,
-    sprites: {
-        background: null,
-        ball: null,
-        platform: null,
-        block: null
-    },
-    init: function() {
-        this.ctx = document.getElementById("mycanvas").getContext("2d");
-        this.setEvents();
-    },
+    platform: null,
+    block: null
+  },
+  init: function() {
+    this.ctx = document.getElementById("mycanvas").getContext("2d");
+    this.setEvents();
+  },
+  setEvents() {
+    window.addEventListener("keydown", e => {
+        if (e.keyCode === KEYS.SPACE) {
+            this.platform.fire();
+        } else if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
+            this.platform.start(e.keyCode);
+        } 
+    });
+    window.addEventListener("keyup", e => {
+        this.platform.stop();
+    });
+  },
+  preload(callback) {
+    let loaded = 0;
+    let required = Object.keys(this.sprites).length;
+    let onImageLoad = () => {
+      ++loaded;
+      if (loaded >= required) {
+        callback();
+      }
+    };
 
-    setEvents() {
-        window.addEventListener("keydown", e => {
-            if (e.keyCode === KEYS.SPACE) {
-                this.platform.fire();
-            } else if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
-                this.platform.start(e.keyCode);
-            }
-        });
-
-        window.addEventListener("keyup", e => {
-            this.platform.stop();
-        });
-    },
-
-    preload(callback) {
-        let loaded = 0;
-        let required = Object.keys(this.sprites).length;
-        let onImageLoad = () => {
-            ++loaded;
-            if (loaded >= required) {
-                callback();
-            }
-        };
-
-        for (let key in this.sprites) {
-            this.sprites[key] = new Image();
-            this.sprites[key].src = "img/" + key + ".png";
-            this.sprites[key].addEventListener("load", onImageLoad);
-        }
-    },
-
-    create() {
-        for (let row = 0; row < this.rows; row++) {
-            for (let col = 0; col < this.cols; col++) {
-                this.blocks.push({
-                    width: 60,
-                    height: 20,
-                    x: 64 * col + 65,
-                    y: 24 * row + 35
-                });
-            }
-        }
-    },
-
-    update() {
-        this.platform.move();
-        this.ball.move();
-
-        for (let block of this.blocks) {
-            if (this.ball.collide(block)) {
-                this.ball.bumpBlock(block);
-            }
-        }
-    },
-
-    run() {
-        window.requestAnimationFrame(() => {
-            this.update();
-            this.render();
-            this.run();
-        });
-    },
-
-    render() {
-        this.ctx.clearRect(0, 0, this.width, this.height);
-        this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
-            this.ball.x, this.ball.y, this.ball.width, this.ball.height);
-        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
-        this.renderBlocks();
-    },
-
-    renderBlocks() {
-        for (let block of this.blocks) {
-            this.ctx.drawImage(this.sprites.block, block.x, block.y);
-        }
-    },
-
-    start: function() {
-        this.init();
-        this.preload(() => {
-            this.create();
-            this.run();
-        });
-    },
-    random(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
+    for (let key in this.sprites) {
+        this.sprites[key] = new Image();
+        this.sprites[key].src = "img/" + key + ".png";
+        this.sprites[key].addEventListener("load", onImageLoad);
     }
+  },
+  create() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        this.blocks.push({
+          active: true,
+          width: 60,
+          height: 20,
+          x: 64 * col + 65,
+          y: 24 * row + 35
+        });
+      }
+    }
+  },
+  update(){
+    this.collideBlocks();
+    this.collidePlatform();
+    this.ball.collideWorldBounds();
+    this.platform.move();
+    this.ball.move();
+  },
+  collideBlocks(){
+    for (let block of this.blocks) {
+      if (block.active && this.ball.collide(block)) {
+        this.ball.bumpBlock(block);
+      }
+    }
+  },
+  collidePlatform() {
+    if (this.ball.collide(this.platform)) {
+      this.ball.bumpPlatform(this.platform);
+    }
+  },
+  run() {
+    window.requestAnimationFrame(() => {
+        this.update();
+        this.render();
+        this.run();
+    });
+  },
+  render() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.ctx.drawImage(this.sprites.background, 0, 0);
+    this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+    this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+    this.renderBlocks();
+  },
+  renderBlocks() {
+    for (let block of this.blocks){
+      if (block.active)  {
+      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+    }
+    }
+  },
+  start: function() {
+    this.init();
+    this.preload(() => {
+        this.create();
+        this.run();
+    });
+  },
+  random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 };
+
 
 game.ball = {
     dx: 0,
@@ -131,23 +134,58 @@ game.ball = {
             this.y += this.dy;
         }
         if (this.dx) {
-            this.x += this.dx;
+          this.x += this.dx;
         }
     },
     collide(element) {
+      let x = this.x + this.dx;
+      let y = this.y + this.dy;
+
+      if (x + this.width > element.x &&
+        x < element.x + element.width &&
+        y + this.height > element.y &&
+        y < element.y + element.height) {
+          return true;
+        }
+        return false;
+    },
+    collideWorldBounds() {
         let x = this.x + this.dx;
         let y = this.y + this.dy;
 
-        if (x + this.width > element.x &&
-            x < element.x + element.width &&
-            y + this.height > element.y &&
-            y < element.y + element.height) {
-                return true;
-            }
-        return false;    
+        let ballLeft = x;
+        let ballRight = ballLeft + this.width;
+        let ballTop = y;
+        let ballBottom = ballTop + this.height;
+
+        let worldLeft = 0;
+        let worldRight = game.width;
+        let worldTop = 0;
+        let worldBottom = game.height;
+
+        if (ballLeft < worldLeft) {
+          this.x = 0;
+          this.dx = this.velocity;
+        } else if (ballRight > worldRight) {
+          this.x = worldRight - this.width;
+          this.dx = -this.velocity;
+        } else if (ballTop < worldTop) {
+          this.y = 0;
+          this.dy = this.velocity;
+        } else if (ballBottom > worldBottom) {
+          console.log('game over');
+        }
     },
     bumpBlock(block) {
-        this.dy *= -1;
+      this.dy *= -1;
+      block.active = false;
+    },
+    bumpPlatform(platform) {
+      if (this.dy > 0) {
+        this.dy = -this.velocity;
+        let touchX = this.x + this.width / 2;
+      this.dx = this.velocity * platform.getTouchOffset(touchX);
+      }    
     }
 };
 
@@ -156,21 +194,23 @@ game.platform = {
     dx: 0,
     x: 280,
     y: 300,
+    width: 100,
+    height: 14,
     ball: game.ball,
-    fire() {
+    fire(){
         if (this.ball) {
             this.ball.start();
             this.ball = null;
         }
     },
-    start(direction) {
+    start(direction){
         if (direction === KEYS.LEFT) {
             this.dx = -this.velocity;
         } else if (direction === KEYS.RIGHT) {
             this.dx = this.velocity;
         }
     },
-    stop() {
+    stop(){
         this.dx = 0;
     },
     move() {
@@ -180,9 +220,14 @@ game.platform = {
                 this.ball.x += this.dx;
             }
         }
+    },
+    getTouchOffset(x) {
+      let diff = (this.x + this.width) - x;
+      let offset = this.width - diff;
+      let result = 2 * offset / this.width;
+      return result - 1;
     }
 };
-
 window.addEventListener("load", () => {
     game.start();
 });
