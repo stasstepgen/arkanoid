90d8b0eeb8872e7cbda7806ccdc4cb74d213042b
Stanislav Stepanov <stanislav.stepanov@ivkhk.ee>
2025-12-15 17:48:14 +0200

    17

--- game.js
+++ game.js
@@ -26,7 +26,12 @@ let game = {
   },
   init() {
     this.ctx = document.getElementById("mycanvas").getContext("2d");
+    this.setTextFont();
     this.setEvents();
+  },
+  setTextFont() {
+    this.ctx.font = "20px Arial";
+    this.ctx.fillStyle = "#FFFFFF";
   },
   setEvents() {
     window.addEventListener("keydown", e => {
@@ -126,9 +131,10 @@ let game = {
     this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
     this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
     this.renderBlocks();
+    this.ctx.fillText("Score: " + this.score, 15, 20);
   },
   renderBlocks() {
-    for (let block of this.blocks){
+    for (let block of this.blocks) {
       if (block.active)  {
         this.ctx.drawImage(this.sprites.block, block.x, block.y);
       }
