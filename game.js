7f07ad222857480d1ed78761704c92bd3796df73
Stanislav Stepanov <stanislav.stepanov@ivkhk.ee>
2025-12-15 17:31:53 +0200

    13

--- game.js
+++ game.js
@@ -68,6 +68,7 @@ let game = {
     this.collideBlocks();
     this.collidePlatform();
     this.ball.collideWorldBounds();
+    this.platform.collideWorldBounds();
     this.platform.move();
     this.ball.move();
   },
@@ -181,6 +182,10 @@ game.ball = {
       block.active = false;
     },
     bumpPlatform(platform) {
+      if (platform.dx) {
+        this.x += platform.dx;
+      }
+
       if (this.dy > 0) {
         this.dy = -this.velocity;
         let touchX = this.x + this.width / 2;
@@ -226,6 +231,17 @@ game.platform = {
       let offset = this.width - diff;
       let result = 2 * offset / this.width;
       return result - 1;
+    },
+    collideWorldBounds() {
+      let x = this.x + this.dx;
+      let platformLeft = x;
+      let platformRight = platformLeft + this.width;
+      let worldLeft = 0;
+      let worldRight = game.width;
+
+      if (platformLeft < worldLeft || platformRight > worldRight) {
+        this.dx = 0;
+      }
     }
 };
 window.addEventListener("load", () => {
